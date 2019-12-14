9f3f137cb36fcce3191a1df9f433ed6b6ffcc305
CreasyDev <franckgnaika@gmail.com>
2019-12-13 19:32:01 -0500

    Envoi3 du Chapitre 3 les 5 premiers exercices inclus le 13.12.2019

--- Programmation1/Chapitre3/js/exo4.js
+++ Programmation1/Chapitre3/js/exo4.js
@@ -2,8 +2,8 @@
 /* Affichez 10 fois un décompte de 20 à 2. */
 
 for (var repeat =  0; repeat < 10; repeat++) {
-    for (var i = 20; i >= 2; i--) {
-        document.write ('Affiche moi' + i + '<br>');
+    for (var i = 20; i > 2; i--) {
+        document.write ('Affiche moi ' + i + '<br>');
     }
 }
 
