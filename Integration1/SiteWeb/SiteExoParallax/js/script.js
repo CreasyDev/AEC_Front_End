91d3425ca72ba4aceb9989c9cdc376574a255757
CreasyDev <franckgnaika@gmail.com>
2020-03-10 21:04:08 -0400

    Mise à jour dossier SiteExoParallax le 10.03.2020

--- Integration1/SiteWeb/SiteExoParallax/js/script.js
+++ Integration1/SiteWeb/SiteExoParallax/js/script.js
@@ -2,7 +2,7 @@
 // $(window).scroll(function(){
 //    var scroll_position = $(window).scrollTop()/2;
 //
-//    $('.parallax1, .parallax3').css({
+//    $('.parallax1').css({
 //       'background-position-y' : - scroll_position + 'px',
 //    });
 // });
