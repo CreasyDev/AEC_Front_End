6c62863380c894c5e5c91ed990efaff16fe23fe3
CreasyDev <franckgnaika@gmail.com>
2020-06-15 14:38:19 -0400

    Envoi 4 Projet Spotify le 15.06.2020

---
+++ Programmation2React/spotify/build/precache-manifest.4715522e084b1d49594b03e339cb4dcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68b408bd5f14f565697ba570e38261c5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b154c47f5994d526113",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6594c1a7c25d4ee43d62",
+    "url": "./static/css/main.3e6f6867.chunk.css"
+  },
+  {
+    "revision": "3b154c47f5994d526113",
+    "url": "./static/js/2.2e49dca3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.2e49dca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6594c1a7c25d4ee43d62",
+    "url": "./static/js/main.6acc7f64.chunk.js"
+  },
+  {
+    "revision": "334262939d54c64b02df",
+    "url": "./static/js/runtime-main.44f565e9.js"
+  }
+]);
