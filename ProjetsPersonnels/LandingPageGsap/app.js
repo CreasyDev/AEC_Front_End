0f8d09c983a2564373cd349b182f62053686ecc0
metaTron333 <franckgallardnaika@gmail.com>
2020-11-05 14:52:23 -0500

    Ajout ProjetsPersos le 05.11.2020

---
+++ ProjetsPersonnels/LandingPageGsap/app.js
@@ -0,0 +1,34 @@
+// MAIN GREENSOCK JS
+
+// Constantes
+const banniere = document.querySelector('.banniere');
+const slider = document.querySelector('.background');
+const logo = document.querySelector('#logo');
+const hamburger = document.querySelector('.icon');
+const titre = document.querySelector('.titre');
+const btnClose = document.querySelector('.closeBtn');
+const nav = document.getElementById('myNav');
+const lettres = document.querySelectorAll('.lettres');
+const navLink = document.querySelectorAll('.navLink');
+
+
+// Création de Timeline
+const tl = new TimelineMax();
+
+tl
+.fromTo(banniere, 1, {height:"0%"}, {height:"80%", ease: Power2.easeInOut})
+.fromTo(banniere, 1.2, {width:"100%"}, {width:"80%", ease: Power2.easeInOut})
+.fromTo(slider, 1.2, {x:"-100%"}, {x:"0%", ease: Power2.easeInOut})
+.fromTo(hamburger, 2, {rotation: 0, opacity: 0, x: "-2000%"}, {rotation: 720, opacity: 1, x: "0%", ease: Power2.easeInOut}, "-=1.2")
+.from(logo, 1, {x: "-100%", ease: Back.easeOut.config(1,7)}, "-=2")
+.staggerFrom(lettres, 1, {opacity: 0, ease: Back.easeOut.config(1,7)}, 0.2, "-=0.5");
+
+
+hamburger.addEventListener('click', function(){
+    TweenMax.to(nav, 0.2, {right: 0});
+})
+
+
+btnClose.addEventListener('click', function(){
+    TweenMax.to(nav, 0.1, {right: -300});
+})
